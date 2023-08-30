39ba00ccc623c125d019095e8f210cd35ceed47c
Alik Nigmatullin <NigAlik020503@yandex.ru>
2023-08-30 14:19:09 +0300

    refactored structure of base

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 
 app.get("/api/regions", function (request, response) {
-  const content = readFileSync("./base/regions.json","utf8");
+  const content = readFileSync("./base/page-0/regions.json","utf8");
   const regions = JSON.parse(content);
 
   response.send(regions);
@@ -15,19 +15,19 @@ app.get("/api/regions", function (request, response) {
 
 // Page-1
 app.get("/api/site-preparation-works", function (request, response) {
-  const content = readFileSync("./base/sitePreparationWorks.json","utf8");
+  const content = readFileSync("./base/page-1/sitePreparationWorks.json","utf8");
   const jobs = JSON.parse(content);
 
   response.send(jobs);
 });
 app.get("/api/works-on-the-site", function (request, response) {
-  const content = readFileSync("./base/worksOnTheSite.json","utf8");
+  const content = readFileSync("./base/page-1/worksOnTheSite.json","utf8");
   const jobs = JSON.parse(content);
 
   response.send(jobs);
 });
 app.get("/api/design-and-project-of-the-house", function (request, response) {
-  const content = readFileSync("./base/designAndProjectOfTheHouse.json","utf8");
+  const content = readFileSync("./base/page-1/designAndProjectOfTheHouse.json","utf8");
   const jobs = JSON.parse(content);
 
   response.send(jobs);
@@ -35,7 +35,7 @@ app.get("/api/design-and-project-of-the-house", function (request, response) {
 
 // Page-2
 app.get("/api/base-types", function (request, response) {
-  const content = readFileSync("./base/baseTypes.json","utf8");
+  const content = readFileSync("./base/page-2/baseTypes.json","utf8");
   const baseTypes = JSON.parse(content);
 
   response.send(baseTypes);
@@ -45,7 +45,7 @@ app.get("/api/base-types", function (request, response) {
 
 // базу ещё не заполнил
 app.get("/api/wall-material", function (request, response) {
-  const content = readFileSync("./base/wallMaterial.json","utf8");
+  const content = readFileSync("./base/page-3/wallMaterial.json","utf8");
   const baseTypes = JSON.parse(content);
 
   response.send(baseTypes);
